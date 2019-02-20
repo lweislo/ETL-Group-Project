7f7a729d36b33428376d9d12aa51686ee69b8d30
L Weislo <43381333+lweislo@users.noreply.github.com>
2019-02-19 22:24:39 -0500

    I told gitignore to ignore you

--- config_env.py
+++
@@ -1,3 +0,0 @@
-api_key = "SrHpw0UozDxB5L0HPQbQXTM9n89lcikp"
-
-test_url="https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=LptynSqmgrYWGBusejIrH6N4Ci8tHqD5&q=venezuela&begin_date=20140101&end_date=20190218"
