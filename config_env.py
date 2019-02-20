c21c48a27153603d694477c1a53383b77c3d58b0
coffngrl <coffngrl@gmail.com>
2019-02-19 22:22:00 -0500

    extracted 100 pages of json from NYT

---
+++ config_env.py
@@ -0,0 +1,3 @@
+api_key = "SrHpw0UozDxB5L0HPQbQXTM9n89lcikp"
+
+test_url="https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=LptynSqmgrYWGBusejIrH6N4Ci8tHqD5&q=venezuela&begin_date=20140101&end_date=20190218"
