7a7669908183b9217601a4f69540ac6beacc6ccc
coffngrl <coffngrl@gmail.com>
2019-02-22 12:41:22 -0500

    uploaded local db into cloud, tweaked code

--- config_env.py
+++ config_env.py
@@ -1,2 +1,4 @@
-api_key = "SrHpw0UozDxB5L0HPQbQXTM9n89lcikp"
+api_key = "LptynSqmgrYWGBusejIrH6N4Ci8tHqD5"
 g_key = "233241d4-f6fa-4248-8c0d-25f83912280b"
+username = "laura_weislo:"
+password = "Merlotlegsapple13&"
